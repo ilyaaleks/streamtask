92ba2bafc5f6f89ca56f52823c0c742f3a13cc2e
igorbetron <igorrabcew16071994@gmail.com>
2022-01-14 16:07:39 +0300

    Created student service

---
+++ src/main/java/com/leverx/streamtasks/task1/services/StudentService.java
@@ -0,0 +1,9 @@
+package com.leverx.streamtasks.task1.services;
+
+import com.leverx.streamtasks.task1.entities.Student;
+import java.util.List;
+
+public interface StudentService {
+
+  List<Student> getStudents();
+}
