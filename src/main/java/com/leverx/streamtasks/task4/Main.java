1423cbe5ed5895f083349d954d6b280919ea4da4
igorbetron <igorrabcew16071994@gmail.com>
2022-01-17 10:33:18 +0300

    Updated main method: call service methods and print result to display

--- src/main/java/com/leverx/streamtasks/task4/Main.java
+++ src/main/java/com/leverx/streamtasks/task4/Main.java
@@ -1,8 +1,16 @@
 package com.leverx.streamtasks.task4;
 
+import com.leverx.streamtasks.task4.services.CharacterServiceImpl;
+
 public class Main {
 
   public static void main(String[] args) {
+    var a = "a4a4a4a4";
+    var characterService = new CharacterServiceImpl();
 
+    var charsAndCount = characterService.getCharsAndCountInString(a);
+    var frequentCharInMap = characterService.getMaxCharAndCount(charsAndCount);
+
+    System.out.println(frequentCharInMap);
   }
 }
