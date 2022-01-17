1205b2fd2716b8e81cfb7d66ae48c90cec6c2870
igorbetron <igorrabcew16071994@gmail.com>
2022-01-17 09:57:12 +0300

    Created character service

---
+++ src/main/java/com/leverx/streamtasks/task4/services/CharacterServiceImpl.java
@@ -0,0 +1,23 @@
+package com.leverx.streamtasks.task4.services;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CharacterServiceImpl implements CharacterService {
+
+  @Override
+  public Map<Character, Long> getCharsAndCountInString(String target) {
+    return target.chars()
+        .mapToObj(codeOfCharacter -> (char) codeOfCharacter)
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+  }
+
+  @Override
+  public Entry<Character, Long> getMaxCharAndCount(Map<Character, Long> target) {
+    return target.entrySet().stream()
+        .max((e1, e2) -> e1.getValue().compareTo(e2.getValue())).get();
+  }
+}
